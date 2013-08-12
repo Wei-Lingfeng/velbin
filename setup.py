222673be268621a85cba831e6f88d2b8ae74aace
Michiel Cottaar <MichielCottaar@gmail.com>
2013-08-12 18:30:26 +0200

    Version 1.2: * bugfixes

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ config = {
     'url': 'https://github.com/MichielCottaar/velbin',
     'download_url': 'https://github.com/MichielCottaar/velbin',
     'author_email': 'MichielCottaar@gmail.com',
-    'version': '0.11',
+    'version': '0.12',
     'install_requires': ['scipy'],
     'packages': ['velbin'],
     'scripts': [],
